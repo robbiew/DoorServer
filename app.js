f2a70ac94e5f2dd50d0916860c6dc78367bc92d7
robbiew <robbiew@gmail.com>
2024-11-22 09:58:49 -0800

    don't disconnect debug menu users after game exit

--- app.js
+++ app.js
@@ -1,95 +1,86 @@
-const net = require('net')
-const ConnectionManager = require('./lib/connection_manager')
-const InputHandler = require('./lib/input_handler')
-const config = require('./config')
+const net = require('net');
+const ConnectionManager = require('./lib/connection_manager');
+const InputHandler = require('./lib/input_handler');
+const config = require('./config');
 
 process.on('uncaughtException', err => {
-  console.log(err.stack)
-})
+  console.error('Uncaught Exception:', err.stack || err);
+});
 
-process.on('unhandledRejection', err => {
-  console.log(err.stack)
-})
+process.on('unhandledRejection', reason => {
+  console.error('Unhandled Rejection:', reason.stack || reason);
+});
 
-start()
+start();
 
 async function start() {
   try {
-    validateConfig()
-    startServer()
-    console.log('DoorNode listening on port ' + config.port)
+    validateConfig();
+    startServer();
+    console.log(`DoorNode listening on port ${config.port}`);
     if (config.debugPort) {
-      startDebugServer()
-      console.log('Debug interface listening on port ' + config.debugPort)
+      startDebugServer();
+      console.log(`Debug interface listening on port ${config.debugPort}`);
     }
   } catch (err) {
-    console.log(err.stack)
-    process.exit()
+    console.error(err.stack || err);
+    process.exit(1);
   }
 }
 
 function validateConfig() {
-  if (!config.port) {
-    throw new Error('config.port not configured')
-  }
-
-  if (!config.dosbox.dosboxPath) {
-    throw new Error('config.dosbox.dosboxPath not configured')
-  }
-
-  if (!config.dosbox.configPath) {
-    throw new Error('config.dosbox.configPath not configured')
-  }
-
-  if (!config.dosbox.drivePath) {
-    throw new Error('config.dosbox.drivePath not configured')
-  }
-
-  if (!config.dosbox.startPort) {
-    throw new Error('config.dosbox.startPort not specified')
-  }
+  if (!config.port) throw new Error('config.port not configured');
+  if (!config.dosbox.dosboxPath) throw new Error('config.dosbox.dosboxPath not configured');
+  if (!config.dosbox.configPath) throw new Error('config.dosbox.configPath not configured');
+  if (!config.dosbox.drivePath) throw new Error('config.dosbox.drivePath not configured');
+  if (!config.dosbox.startPort) throw new Error('config.dosbox.startPort not specified');
 }
 
 function startServer() {
   let server = net.createServer(conn => {
-    conn.setEncoding('binary')
-    
-    let wrapper = ConnectionManager.init(conn)
+    conn.setEncoding('binary');
+
+    // Pass isDebugConnection as false for regular connections
+    let wrapper = ConnectionManager.init(conn, false);
 
     conn.on('end', () => {
-      ConnectionManager.close(wrapper)
-    })
+      ConnectionManager.close(wrapper);
+    });
 
     conn.on('data', async str => {
-      await InputHandler.onData(wrapper, str)
-    })
-  })
+      await InputHandler.onData(wrapper, str);
+    });
+  });
 
-  server.listen(config.port)
+  server.listen(config.port, () => {
+    console.log(`Server listening on port ${config.port}`);
+  });
 }
 
 function startDebugServer() {
   let server = net.createServer(conn => {
-    conn.setEncoding('binary')
-    
-    let wrapper = ConnectionManager.init(conn)
-    wrapper.user = {
-      name: 'DebugUser',
-      module: 'Debug',
-      terminal: 'ansi'
-    }
+    conn.setEncoding('binary');
 
-    wrapper.setDebugModule()
+    // Pass isDebugConnection as true for debug connections
+    let wrapper = ConnectionManager.init(conn, true);
+    wrapper.user = {
+      name: config.debugUser?.name || 'DebugUser',
+      module: config.debugUser?.module || 'Debug',
+      terminal: config.debugUser?.terminal || 'ansi',
+    };
+
+    wrapper.setDebugModule();
 
     conn.on('end', () => {
-      ConnectionManager.close(wrapper)
-    })
+      ConnectionManager.close(wrapper);
+    });
 
     conn.on('data', async str => {
-      await InputHandler.onData(wrapper, str)
-    })
+      await InputHandler.onData(wrapper, str);
+    });
+  });
 
-  })
-
-  server.listen(config.debugPort)
+  server.listen(config.debugPort, () => {
+    console.log(`Debug interface listening on port ${config.debugPort}`);
+  });
 }
